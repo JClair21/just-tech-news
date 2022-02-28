29c5b1881291763c84023d2505e4070fc0d5cbca
JClair21 <jessicagoodling@gmail.com>
2022-02-27 19:56:03 -0500

    updated code

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 //imports the 'User' model
 const User = require('./User');
+const Post = require("./Post");
 
 //exports User model properties????- pretty sure this is it...
 module.exports = { User };
