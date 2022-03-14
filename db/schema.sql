0f208e0f9cb77b765b86627da198877b57c69ae1
JClair21 <jessicagoodling@gmail.com>
2022-03-13 22:02:30 -0400

    added tests

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
 DROP DATABASE IF EXISTS just_tech_news_db;
 
 CREATE DATABASE just_tech_news_db;
+
+CREATE TABLE parties (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT
+);
